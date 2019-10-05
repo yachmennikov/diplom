62ae5635dd36f08babb867cba15ae05c2acd3939
Yachmennikov Sergey <otsmmm2013@gmail.com>
2019-10-05 10:08:58 +0300

    diplom v.1.0

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import 'nodelist-foreach-polyfill';
+import 'babel-polyfill';
+import elementClosest from 'element-closest';
+elementClosest(window);
+import 'formdata-polyfill';
+import 'es6-promise';
+import 'fetch-polyfill';
+
+
+import countTimer from './modules/timer';
+
+
+
+// burger menu
+toggleMenu();
+
